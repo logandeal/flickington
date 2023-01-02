6bd20b494200f8279304f3218419531812cd2822
Logan Deal <logan.deal27@gmail.com>
2023-01-01 23:08:15 -0600

    styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+"use client";
+import { css } from "@emotion/react";
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
+import {
+  Button,
+  Skeleton,
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+  Box,
+} from "@mui/material";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -89,6 +106,74 @@ async function getRandomMovie(
   }
 }
 
+export default function Home() {
+  return (
+    <>
+      {/* <Head>
+        <title>Flickington</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <main>
+        <Typography variant="h1">Flickington</Typography>
+        <Button variant="contained">Get Random Movie</Button>
+        <br />
+        <img src="/movie-reveal.png" height="75.985" width="182" />
+      </main> */}
+      <Head>
+        <title>Flickington</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <CssBaseline />
+      <AppBar position="relative" sx={{ p: 1, alignItems: "center" }}>
+        <Toolbar variant="dense">
+          <img src="/logo-white.png" height="68" width="94" />
+          <Typography variant="h3" fontWeight={600} sx={{ ml: 1 }}>
+            Flickington
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main
+        css={css`
+          height: calc(100vh - 68px - 16px);
+        `}
+      >
+        <div
+          css={css`
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+          `}
+        >
+          <Card sx={{ margin: "auto", maxWidth: 345 }}>
+            <CardContent>
+              <Typography
+                variant="h6"
+                color="text.secondary"
+                sx={{ textAlign: "center" }}
+              >
+                Find your next movie to watch!
+              </Typography>
+              <div
+                css={css`
+                  display: flex;
+                  justify-content: center;
+                  margin-top: 10px;
+                `}
+              >
+                <Button variant="contained">Reveal Movie</Button>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      </main>
+    </>
+  );
+}
+
+/*
 export default async function Home() {
   const latestMovieId = (await getLatestMovie()).id;
 
@@ -103,7 +188,7 @@ export default async function Home() {
       </Head>
       <main>
         <h1>Flickington</h1>
-        {movie.poster_path && (
+        {movie.poster_path ? (
           <>
             <img
               src={`https://www.themoviedb.org/t/p/w188_and_h282_bestv2${movie.poster_path}`}
@@ -113,6 +198,8 @@ export default async function Home() {
             <br />
             <img src="/movie-reveal.png" height="75.985" width="182" />
           </>
+        ) : (
+          <Skeleton variant="rectangular" width={282} height={182} />
         )}
         <h2>{movie.title}</h2>
         {movie.release_date && (
@@ -128,3 +215,4 @@ export default async function Home() {
     </>
   );
 }
+*/
